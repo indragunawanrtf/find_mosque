80f50b1a34f4eea25d3293b994bd14ca76f5896c
Erick Martin <erick.martin@domikado.com>
2017-08-26 22:04:52 +0700

    fix seed admin

--- db/seeds/00-admins.rb
+++ db/seeds/00-admins.rb
@@ -1,11 +1,10 @@
-module SeedUser
+module SeedAdmin
   def self.seed
     new_user            = User.find_or_initialize_by(email: 'admin@site.com')
     new_user.full_name  = 'Admin Test'
     new_user.username   = 'admin'
-
-    new_user.type                  = 'Admin'
-    new_user.organization_ids      = Organization.where(name: value['organization']).pluck(:id) rescue nil
+    new_user.type       = 'Admin'
+    new_user.phone      = '085603155491'
     new_user.password              = '123123123'
     new_user.password_confirmation = '123123123'
     new_user.save
