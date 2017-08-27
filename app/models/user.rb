0827251c7da8195fc1995fe7cfbde1e93df01b88
Erick Martin <erick.martin@domikado.com>
2017-08-27 23:01:26 +0700

    update user device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, :lockable,
+  # :confirmable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,  :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 end
